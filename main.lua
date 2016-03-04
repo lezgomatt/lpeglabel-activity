7bb14e1665a229bbffe8aed9185e152498d22c9c
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-03-05 00:27:11 +0800

    Added a cli with a repl

---
+++ main.lua
@@ -0,0 +1,35 @@
+#!/usr/bin/env lua
+
+local parser = require "parser"
+local interp = require "interpreter"
+
+if arg[1] then
+  local file = io.open(arg[1], "r")
+  local code = file:read("*all")
+  file:close()
+
+  local ast = parser.parse(code)
+  if not ast then
+    print("Error: syntax")
+  else
+    interp.eval(ast)
+  end
+else
+  print("Type `quit` to end the session.")
+
+  local env = interp.new_env()
+
+  repeat
+    io.write("> ")
+
+    local line = io.read()
+    if line == "quit" then break end
+
+    local ast = parser.parse(line)
+    if not ast then
+      print("Error: syntax")
+    else
+      interp.eval(ast, env)
+    end
+  until false
+end
